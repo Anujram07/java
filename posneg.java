8bbeacdc2b19df0d8cc3925854781878f60797b8
Anujram07 <anujramraika12@gmail.com>
2025-04-11 08:33:22 +0530

    java

--- posneg.java
+++ posneg.java
@@ -1,11 +1,15 @@
 public class posneg {
     public static void main(String[] args) {
-        int num = -10;
+        int num = 0;
         if(num>0){
             System.out.println("positive");
         }
-        else{
+        else if (num<0){
             System.out.println("negative");
+        }
+
+        else{
+            System.out.println("zero");
         }
     }
     
