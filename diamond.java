d1b1430dc3bdba892546c9e2d8bfac5995729d40
Anujram07 <anujramraika12@gmail.com>
2025-04-16 13:58:00 +0530

    java

--- diamond.java
+++ diamond.java
@@ -17,7 +17,7 @@ public class diamond {
             System.out.println();
             }
 
-            for(int i=1;i<=5;i++){
+            for(int i=2;i<=5;i++){
                 System.out.println(" ");
                 for( int j=1;j<=i;j++){
                    
