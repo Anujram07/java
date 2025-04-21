1d29257c37ef852a9cdfefe315bb9d81a193aa5d
Anujram07 <anujramraika12@gmail.com>
2025-04-21 14:17:30 +0530

    java

--- sumarray2.java
+++ sumarray2.java
@@ -1,10 +1,15 @@
-public class _2darray {
+/*sum of all elements */
+
+
+public class sumarray2 {
     public static void main(String[] args) {
             int[][] arr={ {1,2,3,4},{1,2,3,4}};
+            int sum=0;
             for(int i=0; i<arr.length;i++){
                 for(int j=0;j<arr[i].length;j++){
-                    System.out.println(arr[i][j]);
-                }
-            }
+                   sum+=arr[i][j];
+                   System.out.println(sum);
+    }
+}
     }
 }
